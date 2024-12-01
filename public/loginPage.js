c398072e517c908fa83029a573b60118694d5f35
Софья Шандыбина <barsuk2001@gmail.com>
2024-12-01 18:02:25 +0300

    login_fixed

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,22 +2,13 @@
 
 const newForm = new UserForm();
 
-const loginForm = document.getElementById('login');
-
-const registerForm = document.getElementById('register');
-
-loginForm.addEventListener('submit', newForm.loginFormAction);
-
-registerForm.addEventListener('submit', newForm.registerFormAction);
-
 
 newForm.loginFormCallback = function(data) {
     ApiConnector.login(data, serverCheck);
 
     function serverCheck(actionResult) {
         if (!actionResult.success) {
-            const message = actionResult.error
-            newForm.setLoginErrorMessage(message)
+            newForm.setLoginErrorMessage(actionResult.error)
         } else {
             location.reload();
         }
@@ -29,8 +20,7 @@ newForm.registerFormCallback = function(data) {
 
     function serverCheck(actionResult) {
         if (!actionResult.success) {
-            const message = actionResult.error
-            newForm.setRegisterErrorMessage(message)
+            newForm.setRegisterErrorMessage(actionResult.error)
         } else {
             location.reload();
         }
